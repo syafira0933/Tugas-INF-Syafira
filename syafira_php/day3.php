0a030250520da3cf52d3e15c13bd562cc81ed9a9
Firfirra <syafiraputricahayati09@gmail.com>
2025-02-19 08:03:53 +0700

    Add files via upload

---
+++ syafira_php/day3.php
@@ -0,0 +1,58 @@
+<?php 
+$nilai = 83;
+
+if($nilai <= 100 && $nilai >= 90)
+{
+    echo "A";
+}
+else if($nilai <= 89 && $nilai >= 80)
+{
+    echo "B";
+}
+else if($nilai <= 79 && $nilai >= 70)
+{
+    echo "C";
+} 
+else if ($nilai <= 69 && $nilai > 0) {
+    echo "D";
+}
+else {
+    echo "nilai wajib di atas 0";
+}
+?>
+
+<br> 
+
+<?php 
+$jam = date ("24:30:00");
+
+echo "Sekarang menunjukkan pukul = $jam<br>";
+
+if($jam >= date ("00:00") && $jam < date ("04:00"))
+{
+    echo $jam = "Dini hari";
+}
+else if($jam >= date ("04:00:00") && $jam <= date ("10:00:00"))
+{
+    echo $jam = "Pagi";
+}
+else if($jam >= date ("10:00:00")  && $jam <= date ("15:00:00"))
+{
+    echo $jam = "Siang";
+}
+else if($jam >= date ("15:00:00") && $jam <= date ("18:00:00"))
+{
+    echo $jam = "Sore";
+}
+else if($jam >= date ("17:30:00") && $jam <= date ("18:30:00"))
+{
+    echo $jam = "Petang";
+}
+else if($jam >= date ("18:30:00") && $jam <= date ("24:00:00"))
+{
+    echo $jam = "Malam";
+}
+else {
+    echo $jam = "Dunia lain";
+}
+?>
